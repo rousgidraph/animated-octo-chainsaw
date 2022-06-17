a17ef21db20ee8e9d8777a4f0c7197b409bda159
rousgiraph <rousgidraph@gmail.com>
2022-06-17 07:09:25 +0300

    Was able to log on file drop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DndDirective } from './dnd.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DndDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/dnd.directive.ts
@@ -0,0 +1,50 @@
+import { Directive, EventEmitter, HostBinding, HostListener, Output } from '@angular/core';
+
+@Directive({
+  selector: '[appDnd]'
+})
+export class DndDirective {
+
+  @HostBinding('class.fileover') fileOver: boolean;
+  @Output() fileDropped = new EventEmitter<any>();
+
+  constructor() { }
+
+
+  //dragover listener
+  @HostListener('dragover',['$event']) onDragOver(evt: Event){
+    evt.preventDefault();
+    evt.stopPropagation();
+    this.fileOver = true ;
+
+
+    console.log('drag over ');
+  }
+
+  //Dragleaver listener
+  @HostListener('dragleave',['$event']) public onDragLeave(evt: Event){
+    evt.preventDefault();
+    evt.stopPropagation();
+
+    console.log('drag leave');
+  }
+
+  //drop listener 
+  @HostListener('drop',['$event']) public onDrop(evt : DragEvent){
+    evt.preventDefault();
+    evt.stopPropagation();
+    this.fileOver = false ;
+    let files = evt.dataTransfer!.files;
+
+    if(files.length > 0){
+
+      //do what you have to with the files 
+      
+      console.log(`you have dropped ${files.length} files.`);
+      
+    }
+
+    
+  }
+
+}
