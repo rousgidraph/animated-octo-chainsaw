e0f5852758ccf63d324b33ab64edb927a462d322
rousgiraph <rousgidraph@gmail.com>
2022-06-19 11:57:34 +0300

    Made

--- src/app/dnd.directive.ts
+++ src/app/dnd.directive.ts
@@ -1,5 +1,7 @@
 import { Directive, EventEmitter, HostBinding, HostListener, Output } from '@angular/core';
 
+
+
 @Directive({
   selector: '[appDnd]'
 })
@@ -37,8 +39,19 @@ export class DndDirective {
     let files = evt.dataTransfer!.files;
 
     if(files.length > 0){
-
+      
       //do what you have to with the files 
+      for(var i=0;i<files.length;i++){
+        let curr:File = files[i];
+        var fileReader = new FileReader();
+        fileReader.onload = function(fileLoadedEvent){
+          var textFromFile = fileLoadedEvent.target?.result;
+          console.log(textFromFile);
+          
+        }
+        fileReader.readAsText(curr,"UTF-8");
+
+      }
       
       console.log(`you have dropped ${files.length} files.`);
       
